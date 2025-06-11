6466f9494325a820450884d7d843ee4c1e7ff6e3
Sreeganesh Siva <ssiva@ucsb.edu>
2025-06-11 12:03:55 -0700

    ss - finished lc-20 valid paren

---
+++ lc_0020.py
@@ -0,0 +1,39 @@
+# 20. Valid Parenthesis
+
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+    
+        paren_stack = []
+
+        for char in s:
+            if char == ")":
+                if len(paren_stack) == 0 or paren_stack.pop() != "(":
+                    return False
+            elif char == "]":
+                if len(paren_stack) == 0 or paren_stack.pop() != "[":
+                    return False
+            elif char == "}":
+                if len(paren_stack) == 0 or paren_stack.pop() != "{":
+                    return False
+            else:
+                paren_stack.append(char)
+            # print(paren_stack)
+
+
+        return (len(paren_stack) == 0)
+
+
+sol = Solution()
+print(sol.isValid("()"))
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))
+print(sol.isValid("([])"))
+print(sol.isValid("]"))
+
