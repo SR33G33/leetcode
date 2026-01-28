f9f946ac02e51d3a7e2662bff4fd3a9c2bb697c6
Sreeganesh Siva <ssiva@ucsb.edu>
2026-01-27 16:33:51 -0800

    ss - lc_0981

--- solutions/lc_0981.py
+++ solutions/lc_0981.py
@@ -18,13 +18,32 @@
 class TimeMap:
 
     def __init__(self):
-        
+        self.timeMap = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        
-
+        if(key not in self.timeMap.keys()):
+            self.timeMap[key] = ([], {})
+        timestamps, values = self.timeMap[key]
+        timestamps.append(timestamp)
+        values[timestamp] = value
+    
     def get(self, key: str, timestamp: int) -> str:
-        
+        if key not in self.timeMap: return ""
+        timestamps, values = self.timeMap[key]
+
+        left, right = 0, len(timestamps) - 1
+        res = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+            if timestamps[mid] <= timestamp:
+                res = values[timestamps[mid]]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return res
+
 
 
 # Your TimeMap object will be instantiated and called as such:
